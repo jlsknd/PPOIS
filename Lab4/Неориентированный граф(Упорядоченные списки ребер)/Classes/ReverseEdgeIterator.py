38a4524d2733e81e3a35ff646a247d1f412b3f4c
jlsknd <jlskndglass@gmail.com>
2025-11-15 12:52:45 +0300

    Create ReverseEdgeIterator.py

---
+++ Lab4/Неориентированный граф(Упорядоченные списки ребер)/Classes/ReverseEdgeIterator.py
@@ -0,0 +1,40 @@
+class ReverseEdgeIterator(Generic[T]):
+    """
+    @class ReverseEdgeIterator
+    @brief Обратный двунаправленный итератор для перебора ребер
+    """
+
+    def __init__(self, graph: UndirectedGraph[T], position: int):
+        """
+        @brief Конструктор обратного итератора ребер
+        @param graph Граф
+        @param position Текущая позиция
+        """
+        self._graph = graph
+        self._position = position
+
+    def current(self) -> Edge[T]:
+        """Получает текущее ребро"""
+        if self._position < 0 or self._position >= len(self._graph._edges):
+            raise IndexError("Итератор вышел за пределы")
+        return self._graph._edges[self._position]
+
+    def next(self):
+        """Переходит к следующему ребру (в обратном направлении)"""
+        self._position -= 1
+        return self
+
+    def prev(self):
+        """Переходит к предыдущему ребру (в обратном направлении)"""
+        self._position += 1
+        return self
+
+    def __eq__(self, other) -> bool:
+        """Оператор равенства"""
+        return (isinstance(other, ReverseEdgeIterator) and
+                self._graph is other._graph and
+                self._position == other._position)
+
+    def __ne__(self, other) -> bool:
+        """Оператор неравенства"""
+        return not self.__eq__(other)
