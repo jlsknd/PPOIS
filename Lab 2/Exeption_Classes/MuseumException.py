6a4d45556773687fcf748241fae5696960378700
jlsknd <jlskndglass@gmail.com>
2025-11-15 06:37:38 +0300

    Delete Lab 2/Exeption_Classes directory

--- Lab 2/Exeption_Classes/MuseumException.py
+++
@@ -1,7 +0,0 @@
-class MuseumException(Exception):
-    """Базовое исключение для системы управления музеем."""
-    
-    def __init__(self, message="Ошибка системы управления музеем"):
-        super().__init__(message)
-        self.message = message
-
