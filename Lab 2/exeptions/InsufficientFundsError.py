22ba32ad3981fcc1cc0b700a44c734ca73bdbb29
jlsknd <jlskndglass@gmail.com>
2025-11-15 10:18:37 +0300

    Create InsufficientFundsError.py

---
+++ Lab 2/exeptions/InsufficientFundsError.py
@@ -0,0 +1,6 @@
+class InsufficientFundsError(MuseumException):
+    """Исключение при недостаточных средствах на счету."""
+    
+    def __init__(self, message="Недостаточно средств"):
+        super().__init__(message)
+        self.amount_required = None
