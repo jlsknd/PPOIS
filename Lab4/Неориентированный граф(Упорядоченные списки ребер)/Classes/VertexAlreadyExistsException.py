e4c70ec7f611a9bd1ed3b0272f80a22fc84cf44a
jlsknd <jlskndglass@gmail.com>
2025-11-15 12:50:05 +0300

    Create VertexAlreadyExistsException.py

---
+++ Lab4/Неориентированный граф(Упорядоченные списки ребер)/Classes/VertexAlreadyExistsException.py
@@ -0,0 +1,6 @@
+class VertexAlreadyExistsException(GraphException):
+    """
+    @class VertexAlreadyExistsException
+    @brief Исключение для случая, когда вершина уже существует
+    """
+    pass
